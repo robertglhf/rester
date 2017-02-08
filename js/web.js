77340f493b17f5024fb0a559bdd359c4f3a86357
RobertG <robyka2009@gmail.com>
2017-02-08 15:38:47 +0200

    yes.

---
+++ js/web.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+
+    //random bg img
+    var images = ['hero.png', 'hero2.png', 'hero3.png']
+    $('.hero-img').css({'background-image': 'url(images/' + images[Math.floor(Math.random()* images.length)]+ ')'});
+
+    //ajax tabs
+    $("#container li a").click(function() {
+ 
+        $("#ajax-content").empty().append("<div id='loading'>Loading...</div>");
+        $("#nav li a").removeClass('current');
+        $(this).addClass('current');
+ 
+        $.ajax({ url: this.href, success: function(html) {
+            $("#ajax-content").empty().append(html);
+            }
+        });
+    return false;
+    });
+
+    $("#ajax-content").empty().append("<div id='loading'><img src='images/loading.gif' alt='Loading' /></div>");
+    $.ajax({ url: 'page_1.html', success: function(html) {
+        $("#ajax-content").empty().append(html);
+        }
+    });
+
+    //tabs selected
+
+    // $('li').click(function(){
+    //     $(this).addClass('selected-item').siblings().removeClass('selected-item');
+    // });
+
+    if ($('.offers ul li').is('.selected-item') || $('.offers ul li').hasClass('.selected-item')) {
+        $('.showcase--fish').css({
+            'display' : 'flex'
+        })
+    }
+});
+
+//nav bg
+
+$(window).scroll(function () {
+    var pScroll = $(this).scrollTop();
+
+    if (scroll >= 50 ) {
+        $('.dsktp').css({
+            'background-color' : 'white',
+        });
+        $('.dsktp ul li a').css({
+             'color' : 'black'
+        });
+    }
+    if (scroll < 50) {
+        $('.dsktp').css({
+            'background-color' : ''
+        });
+        $('.dsktp ul li a').css({
+             'color' : 'white'
+        });
+    }
+
+    //parallax
+
+    $('.parallax').css({
+        'transform' : 'translate(0px,'+pScroll/50 +'%)'
+    })
+
+});
+
+
+
+
+
